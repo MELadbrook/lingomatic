78a085494bc8eff3af472a052ac9720186ccd7e6
laddi <meladbrook@gmail.com>
2020-06-21 20:43:36 -0700

    added subclass CustomPhoneme

--- lingomatic.py
+++ lingomatic.py
@@ -16,30 +16,30 @@ class Language:
             word = word + self.PHONEMES[random.randint(0, len(self.PHONEMES) - 1)]
         return word
 
-'''    def create_word(self, length):
-        word = ''
-        for letter in range(0, length):
-            word = word + Language.PHONEMES[random.randint(0, len(Language.PHONEMES)-1)]
-        return word
-
-    def create_sentence(self, no_of_words, range_of_word_lengths):
+    def create_sentence(self, no_of_words, word_length):
         sentence = ''
-        for word in range(0, no_of_words):
-            sentence = sentence + Language.create_word(random.randint(range_of_word_lengths[0], range_of_word_lengths[1])) + ' '
+        for word in range(no_of_words):
+            sentence = sentence + self.create_word(random.randint(1, word_length)) + ' '
         return sentence
 
-    def create_custom_phonemes(self, rating):
-        #rating = input("Enter an amount of harshness from 0 to 1")
+
+class CustomPhoneme(Language):
+    def __init__(self, rating):
+        super().__init__()
+        self.rating = rating
+        self.phoneme = self.create_custom_phonemes()
+
+    def create_custom_phonemes(self):
         custom_phoneme = []
-        for letter in Language.HARD:
-            if random.uniform(0, 1) < float(rating):
+        for letter in self.HARD:
+            if random.uniform(0, 1) < float(self.rating):
                 custom_phoneme.append(letter)
-        for letter in range(len(custom_phoneme), len(Language.SOFT)):
-            custom_phoneme.append(Language.SOFT[letter])
-        custom_phoneme.extend(Language.VOWELS)
+        for letter in range(len(custom_phoneme), len(self.SOFT)):
+            custom_phoneme.append(self.SOFT[letter])
+        custom_phoneme.extend(self.VOWELS)
         return custom_phoneme
 
-    def create_varied_word(self, length):
+'''    def create_varied_word(self, length):
         word = ''
         for letter in range(0, length):
             word = word + custom[random.randint(0, len(custom)-1)]
@@ -52,9 +52,12 @@ class Language:
         return sentence'''
 
 example = Language()
+example_subclass = CustomPhoneme(0.5)
 
 
 
 print(example.VOWELS)
 print(example.create_word(12))
+print(example.create_sentence(16, 12))
+print(example_subclass.phoneme)
 
